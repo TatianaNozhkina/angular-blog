8989d4050dd89ad85724b82cad6faa87290cbef1
TatianaNozhkina <bokarevat777@gmail.com>
2020-03-15 16:50:29 +0200

    init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyA_nsHpMD86K4JAA9EqfFJrXdmJVC0Bb0k',
+  fbDBUrl: 'https://angular-blog-9bfff.firebaseio.com'
+};
